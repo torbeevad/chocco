15f2bed8b983fc112b85dd74b64056d10a818edc
Alexey Torbeev <torbec@yandex.ru>
2020-12-14 22:43:22 +0300

    add js for burger

---
+++ main.js
@@ -0,0 +1,20 @@
+let burger = document.querySelector('.burger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+let close = document.querySelector('.close');
+
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(function (element) {
+  element.addEventListener('click', toggleMenu);
+})
+
+function toggleMenu() {
+  burger.classList.toggle('burger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-menu');
+  close.classList.toggle('close--active');
+}
+
+burger.addEventListener('click', toggleMenu);
+close.addEventListener('click' , toggleMenu);
