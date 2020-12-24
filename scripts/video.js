37f319fc24a6576a93d4b025049c1f61841f421c
Alexey Torbeev <torbec@yandex.ru>
2020-12-24 11:54:50 +0300

    correct accord

---
+++ scripts/video.js
@@ -0,0 +1,12 @@
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('yt-player', {
+        height: '405',
+        width: '660',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
