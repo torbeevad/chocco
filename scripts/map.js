cfe3005628de641b64a370ab3b4ff1ccabf613a7
Alexey Torbeev <torbec@yandex.ru>
2020-12-27 22:14:04 +0300

    add map

--- scripts/map.js
+++ scripts/map.js
@@ -1,15 +1,32 @@
-ymaps.ready(init);
-function init() {
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [59.93, 30.31],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 12
+let myMap;
+const init = () => {
+    myMap = new ymaps.Map("map", {
+        center: [59.93, 30.30],
+        zoom: 11,
+        controls: [],
     });
 
-}
+    let coords = [
+        [59.94, 30.38],
+        [59.91, 30.50],
+        [59.88, 30.31],
+        [59.97, 30.31],
+    ],
+        myCollection = new ymaps.GeoObjectCollection({}, {
+            draggable: false,
+            iconLayout: 'default#image',
+            iconImageHref: './img/map/marker.svg',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-35, -52]
+        });
+
+    for (let i = 0; i < coords.length; i++) {
+        myCollection.add(new ymaps.Placemark(coords[i]));
+    }
+
+    myMap.geoObjects.add(myCollection);
+
+    myMap.behaviors.disable('scrollZoom');
+};
+
+ymaps.ready(init);
